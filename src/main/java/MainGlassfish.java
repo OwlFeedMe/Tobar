3e850a3b1d5460121899f9b9292cf5427ec9af50
user <user@Carlos>
2017-05-22 22:12:28 -0500

    muchas cosas wuu

--- src/main/java/MainGlassfish.java
+++ src/main/java/MainGlassfish.java
@@ -36,7 +36,7 @@ public class MainGlassfish {
 	Deployer deployer = glassfish.getDeployer();
 
 	// Create a scattered web application.
-	ScatteredArchive archive = new ScatteredArchive("2505",
+	ScatteredArchive archive = new ScatteredArchive("myApp",
 	ScatteredArchive.Type.WAR, new File(webappDirLocation));
         // target/classes directory contains my complied servlets
 	archive.addClassPath(new File("target", "classes"));
